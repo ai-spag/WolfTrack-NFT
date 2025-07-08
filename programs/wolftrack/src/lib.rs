e0b2122cf3495dc818663c47a16277051a17588c
ai-spag <commonperspectives.jr@gmail.com>
2025-07-08 16:27:36 -0400

    Update lib.rs

--- programs/wolftrack/src/lib.rs
+++ programs/wolftrack/src/lib.rs
@@ -1,9 +1,22 @@
+use anchor_lang::prelude::*;
+
+declare_id!("9uCnxtqvRhMH4YPhAGKDyBmhHkG4Ef3p1YKXdVR7d6Qc");
+
+#[program]
+pub mod wolftrack {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        Ok(())
+    }
+}
+
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(
         init,
         payer = user,
-        space = 8 + 32, // 8 for discriminator, 32 for name
+        space = 8 + 32, // 8 bytes for Anchor discriminator, 32 bytes for name
     )]
     pub my_account: Account<'info, MyAccount>,
 
