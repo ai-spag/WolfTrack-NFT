29a5deb117c7486ff1cd78864784072d5c53a9e5
ai-spag <commonperspectives.jr@gmail.com>
2025-07-08 16:41:26 -0400

    Update lib.rs

--- programs/wolftrack/src/lib.rs
+++ programs/wolftrack/src/lib.rs
@@ -6,7 +6,15 @@ declare_id!("9uCnxtqvRhMH4YPhAGKDyBmhHkG4Ef3p1YKXdVR7d6Qc");
 pub mod wolftrack {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String) -> Result<()> {
+        let input_name = name.as_bytes();
+        let my_account = &mut ctx.accounts.my_account;
+
+        if input_name.len() > 32 {
+            return Err(ErrorCode::NameTooLong.into());
+        }
+
+        my_account.name[..input_name.len()].copy_from_slice(input_name);
         Ok(())
     }
 }
@@ -16,7 +24,7 @@ pub struct Initialize<'info> {
     #[account(
         init,
         payer = user,
-        space = 8 + 32, // 8 bytes for Anchor discriminator, 32 bytes for name
+        space = 8 + 32,
     )]
     pub my_account: Account<'info, MyAccount>,
 
@@ -29,4 +37,10 @@ pub struct Initialize<'info> {
 #[account]
 pub struct MyAccount {
     pub name: [u8; 32],
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("The provided name is too long.")]
+    NameTooLong,
 }
